f6ec0d60f90e60f77834456a3be2333a9755a346
kenf1 <103538771+kenf1@users.noreply.github.com>
2025-10-29 09:45:50 -0700

    Data dictionary query

--- src/logic/dataset_query.py
+++ src/logic/dataset_query.py
@@ -10,9 +10,16 @@ def get_dataset_info(api_key: str, datasetid: int) -> requests.Response:
     return requests.get(full_url, headers=headers)
 
 
+# todo: 403 or 500 error
 def get_dataset(api_key: str, datasetid: int) -> requests.Response:
     full_url: str = (
         f"https://services.scicrunch.io/odc/dataset/{datasetid}/?api_key={api_key}"
     )
+    headers = {"accept": "application/json", "x-auth-token": api_key}
+    return requests.get(full_url, headers=headers)
+
+
+def get_data_dict(api_key: str, datasetid: int) -> requests.Response:
+    full_url: str = f"https://services.scicrunch.io/odc/dataset/{datasetid}/data-dictionary/?api_key={api_key}"
     headers = {"accept": "application/json", "x-auth-token": api_key}
     return requests.get(full_url, headers=headers)
